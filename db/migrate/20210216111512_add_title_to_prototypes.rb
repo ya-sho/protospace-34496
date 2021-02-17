eb596008969f25618393398eb000a4a7c1dea32f
ya-sho <78021007+ya-sho@users.noreply.github.com>
2021-02-17 21:04:04 +0900

    タイトル編集

--- db/migrate/20210216111512_add_title_to_prototypes.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToPrototypes < ActiveRecord::Migration[6.0]
-  def change
-    add_column :prototypes, :title, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -44,12 +44,12 @@ ActiveRecord::Schema.define(version: 2021_02_17_030437) do
   end
 
   create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
     t.text "catch_copy", null: false
     t.text "concept", null: false
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "title"
     t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
