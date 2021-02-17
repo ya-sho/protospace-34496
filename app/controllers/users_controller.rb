67e00b91b1b11b6e4a987e8afb122bee02eb4160
ya-sho <78021007+ya-sho@users.noreply.github.com>
2021-02-17 19:59:35 +0900

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
     @name=@user.name
     @profile=@user.profile
-    @occupation=@user.profile
-    @position=@user.profile
+    @occupation=@user.occupation
+    @position=@user.position 
     @prototypes=@user.prototypes
   end
 
